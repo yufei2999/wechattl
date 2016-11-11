dbb44d2dcf2c2dc25d60d97f66143b6e9c2ea65b
temp <yuye2999@126.com>
2016-11-11 17:08:39 +0800

    test

--- src/test/java/com/yufei/test/Test.java
+++ src/test/java/com/yufei/test/Test.java
@@ -1,17 +1,6 @@
 package com.yufei.test;
 
 import com.yufei.wechat.TulingApiProcess;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 /**
  * Created by pc on 2016-10-12.
